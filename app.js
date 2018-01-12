847f5ec30649d1978a94efa703669420f447739f
Vivek Pandey <vivekpandey1596@gmail.com>
2018-01-12 15:16:20 +0530

    logout feature done.

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const lib = require('./lib/serverLib.js');
 const PORT = 8000;
 
 let app = WebApp.create();
+app.use(lib.loadUser);
+app.use(lib.redirectLoggedOutUserToLogin);
 
 app.get('/',(req,res)=>{
   res.redirect('/login.html');
 });
-
 app.post('/login',lib.loginPageHandler);
+app.get('/logout',lib.logoutPageHandler);
+
 app.postProcess(lib.serveStaticPage);
 
 module.exports = app;
