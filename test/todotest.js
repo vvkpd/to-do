0430e8dda646fa82a16eaa1c8e2058957f0cc5a6
Vivek Pandey <vivekpandey1596@gmail.com>
2018-01-13 14:39:59 +0530

    added todo class in todo.js and tested todotest.

--- test/todotest.js
+++ test/todotest.js
@@ -1,15 +1,58 @@
 const Todo = require('../todolib/todo.js');
+const TodoItem = require('../todolib/todoitem.js');
+
 let chai = require('chai');
 let assert = chai.assert;
 
 describe('TodoList',()=>{
   var todo;
   before(function(){
-    item = new Todo('simple todo','this is a sample');
+    todo = new Todo('simple todo','this is a sample');
   })
+
   describe('getTitle() gives title',()=>{
     it('is equal to simple todo',done=>{
-      assert.equal(item.getTitle(),'simple todo');
+      assert.equal(todo.getTitle(),'simple todo');
+      done();
+    })
+  })
+
+  describe('getDescription() gives discription',()=>{
+    it('is equal to "this is a sample"',done=>{
+      assert.equal(todo.getDescription(),'this is a sample');
+      done();
+    })
+  })
+
+  describe('getItems() gives items',()=>{
+    it('is equal to {}',done=>{
+      assert.deepEqual(todo.getItems(),{});
+      done();
+    })
+  })
+
+  describe('addItem() adds item in the items',()=>{
+    it('',done=>{
+      let item = new TodoItem('clean room');
+      todo.addItem(item);
+      assert.deepEqual(todo.getItems(),{'clean room':{item:'clean room',doneStatus:false}});
+      done();
+    })
+  })
+
+  describe('addItem() adds item in the items',()=>{
+    it('items has atleast one item after adding ',done=>{
+      let item = new TodoItem('clean room');
+      todo.addItem(item);
+      assert.deepEqual(todo.getItems(),{'clean room':{item:'clean room',doneStatus:false}});
+      done();
+    })
+  })
+
+  describe('deleteItem() deletes item from the items',()=>{
+    it('items has no item after deleting ',done=>{
+      todo.deleteItem('clean room');
+      assert.deepEqual(todo.getItems(),{});
       done();
     })
   })
--- todolib/todo.js
+++ todolib/todo.js
@@ -1,11 +1,23 @@
 class Todo {
   constructor(title,description) {
     this.title = title;
-    this.description = title;
-    this.items = [];
+    this.description = description;
+    this.items = {};
   }
   getTitle(){
     return this.title;
+  }
+  getDescription(){
+    return this.description;
+  }
+  addItem(item){
+    this.items[item.getItem()] = item;
+  }
+  deleteItem(item){
+    delete this.items[item];
+  }
+  getItems(){
+    return this.items;
   }
 }
 
