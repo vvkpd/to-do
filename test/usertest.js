c4d354d97ff699a950921ca052882507568cf122
rahulPatil <rp7187655@gmail.com>
2018-01-22 11:06:03 +0530

    added coverage directory to .gitignore

--- test/usertest.js
+++ test/usertest.js
@@ -8,37 +8,52 @@ describe('User',()=>{
   })
 
   describe('getAllTodo',()=>{
-    it('gives all todo',done=>{
+    it('gives all todo',()=>{
       assert.deepEqual(ramu.getAllTodo(),{});
-      done();
     })
   })
 
   describe('addTodo()',()=>{
-    it(' adds todo in allTodo',done=>{
+    it('adds todo in allTodo',()=>{
       ramu.addTodo('simple todo','this is a sample',[]);
       assert.deepEqual(ramu.getAllTodo(),{1:{title:'simple todo',description:'this is a sample',items:{}}});
-      done();
     })
   })
 
   describe('deleteTodo()',()=>{
-    it('should delete todo if todo is present',done=>{
+    it('should delete todo if todo is present',()=>{
       ramu.addTodo('simple todo','this is a sample',[]);
       assert.isOk(ramu.deleteTodo(1));
-      done();
     })
 
-    it('should not delete todo if todo is absent',done=>{
+    it('should not delete todo if todo is absent',()=>{
       assert.isNotOk(ramu.deleteTodo(1));
-      done();
     })
   })
 
   describe('generateUniqID',()=>{
-    it('should give 1 when user do not have todo',done=>{
+    it('should give 1 when user do not have todo',()=>{
       assert.equal(ramu.generateUniqID(),1);
-      done();
+    })
+
+    it('should give 2 when user have one todo',()=>{
+      ramu.addTodo("sample","sample file",['one','two']);
+      assert.equal(ramu.generateUniqID(),2);
+    })
+  })
+
+  describe('getTodo',()=>{
+    it('should give specified todo',()=>{
+      ramu.addTodo("sample","sample file",['one','two']);
+      let expected = {
+        title:'sample',
+        description:'sample file',
+        items:
+        { 1:{item:'one', status:false},
+          2:{item:'two', status:false}
+        }
+      }
+    assert.deepEqual(ramu.getTodo(1),expected);
     })
   })
 })
